d0e5464e0bde069f8ec7b96276c25b963086c6cc
Vinicius <viniciusvds-@hotmail.com>
2021-08-28 07:26:36 -0300

    Hook de servico

---
+++ node/clients/index.ts
@@ -0,0 +1,23 @@
+import { IOClients } from '@vtex/api'
+
+import { OMS } from '@vtex/clients'
+
+import Status from './status'
+import Leads from './leads'
+
+// Extend the default IOClients implementation with our own custom clients.
+export class Clients extends IOClients {
+
+  public get status() {
+    return this.getOrSet('status', Status)
+  }
+
+  public get leads() {
+    return this.getOrSet('leads', Leads)
+  }
+
+  public get OMS() {
+    return this.getOrSet('OMS', OMS)
+  }
+  
+}
